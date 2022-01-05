1176908eab8a3f3a175e7a35a90b884b448998f5
wpgreene <wpgreene@ncsu.edu>
2022-01-04 22:31:09 -0500

    Implementing data limit command

--- src/obj_bundler.c
+++ src/obj_bundler.c
@@ -2,26 +2,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 
 #define ARG__INPUT_FILENAME "--in"
 #define ARG__OUTPUT_FILENAME "--out"
 #define ARG__PRESERVE_COMMENTS "--preserve-comments"
 #define ARG__PRESERVE_GROUP_NAMES "--preserve-group-names"
 #define ARG__MTL_TAB_SPACE "--mtl-tab-space="
+#define ARG__SET_DATA_LIMIT "--set-data-limit="
 #define ARG__HELP "--help"
 
 #define _ERROR_NO_INPUT_FILE_PROVIDED 100
 #define _ERROR_NO_OUTPUT_FILE_PROVIDED 101
 #define _ERROR_NO_MTL_SPACE_PROVIDED 102
-#define _ERROR_INVALID_INPUT_FILE_PROVIDED 103
-#define _ERROR_INVALID_OUTPUT_FILE_PROVIDED 104
-#define _ERROR_INVALID_MTL_SPACE_PROVIDED 105
-#define _ERROR_INVALID_COMMAND_PROVIDED 106
+#define _ERROR_NO_DATA_LIMIT_PROVIDED 103
+#define _ERROR_INVALID_INPUT_FILE_PROVIDED 104
+#define _ERROR_INVALID_OUTPUT_FILE_PROVIDED 105
+#define _ERROR_INVALID_MTL_SPACE_PROVIDED 106
+#define _ERROR_INVALID_DATA_LIMIT_PROVIDED 107
+#define _ERROR_EXCESS_DATA_LIMIT_PROVIDED 108
+#define _ERROR_INVALID_COMMAND_PROVIDED 109
 
 #define INPUT_FILE_EXT "obj"
 #define OUTPUT_FILE_EXT "objb"
 #define MIN_MTL_SPACE 3
 #define MAX_MTL_SPACE 10
+
+#define LINE_BUFFER_LEN 100
+#define DEFAULT_DATA_LIMIT 1000000
+#define MAX_DATA_LIMIT 99999999
 
 char *_input_filename;
 char *_output_filename;
@@ -31,6 +40,7 @@ bool _preserve_comments = false;
 bool _preserve_group_names = false;
 
 int _mtl_spacing = MIN_MTL_SPACE;
+unsigned int _data_limit = DEFAULT_DATA_LIMIT;
 
 void _BUNDLER_HELP() {
 	
@@ -130,6 +140,7 @@ int main( int argc, char* argv[] ) {
 		}
 		else {
 			char *mtl_cmd_found = strstr( argv[i], ARG__MTL_TAB_SPACE );
+			
 			if( mtl_cmd_found != NULL ) {
 				int mtl_index = mtl_cmd_found - argv[i];
 				
@@ -162,8 +173,49 @@ int main( int argc, char* argv[] ) {
 				}
 			}
 			else {
-				_BUNDLER_ERROR( _ERROR_INVALID_COMMAND_PROVIDED, argv[ i ] );
-				exit( EXIT_FAILURE );
+				char *sdl_cmd_found = strstr( argv[ i ], ARG__SET_DATA_LIMIT );
+				
+				if( sdl_cmd_found != NULL ) {
+					int sdl_index = sdl_cmd_found - argv[i];
+					
+					if( sdl_index == 0 ) {
+						int sdl_cmd_len = strlen( ARG__SET_DATA_LIMIT );
+						int user_sdl_cmd_len = strlen( argv[ i ] );
+						
+						if( user_sdl_cmd_len == sdl_cmd_len ) {
+							_BUNDLER_ERROR( _ERROR_NO_DATA_LIMIT_PROVIDED, NULL );
+							exit( EXIT_FAILURE );
+						}
+						
+						int limit_len = user_sdl_cmd_len - sdl_cmd_len;
+						
+						char *limit_buffer = (char *) malloc( limit_len );
+						memcpy( limit_buffer, &( argv[ i ][ user_sdl_cmd_len - limit_len ] ), limit_len );
+						limit_buffer[ limit_len ] = '\0';
+						
+						// IF number of digits provided is greater
+						// than the number of digits in the max data
+						// limit, error out
+						if( limit_len > log2( MAX_DATA_LIMIT ) + 1 ) {
+							_BUNDLER_ERROR( _ERROR_EXCESS_DATA_LIMIT_PROVIDED, limit_buffer );
+							exit( EXIT_FAILURE );
+						}
+						
+						_data_limit = atoi( limit_buffer );
+						if( _data_limit > MAX_DATA_LIMIT ) {
+							_BUNDLER_ERROR( _ERROR_INVALID_DATA_LIMIT_PROVIDED, limit_buffer );
+							exit( EXIT_FAILURE );
+						}
+					}
+					else {
+						_BUNDLER_ERROR( _ERROR_INVALID_MTL_SPACE_PROVIDED, NULL );
+						exit( EXIT_FAILURE );
+					}
+				}
+				else {
+					_BUNDLER_ERROR( _ERROR_INVALID_COMMAND_PROVIDED, argv[ i ] );
+					exit( EXIT_FAILURE );
+				}
 			}
 		}
 	}
@@ -194,6 +246,25 @@ int main( int argc, char* argv[] ) {
 			_output_filename[ pf_len + i ] = OUTPUT_FILE_EXT[ i - 1 ];
 		}
 		_output_filename[ pf_len + out_ext_len + 1 ] = '\0';
+	}
+	
+	FILE *in_handle = fopen( _input_filename, "r" );
+	FILE *out_handle = fopen( _output_filename, "w" );
+	
+	if( in_handle == NULL ) {
+		// TODO: IO error
+		exit( EXIT_FAILURE );
+	}
+	
+	char *line_buffer = (char *) malloc( LINE_BUFFER_LEN );
+	while( fgets( line_buffer, LINE_BUFFER_LEN, in_handle ) ) {
+		switch( line_buffer[ 0 ] ) {
+			case 'v':
+				if( line_buffer[ 1 ] == 't' ) {
+					
+				}
+			break;
+		}
 	}
 	
 	return EXIT_SUCCESS;
